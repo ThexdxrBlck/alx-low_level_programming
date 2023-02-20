ca0e53fbdd58d35ec6d9a4780e6fbc58b91531ef
ThexdxrBlck <morobe76o@gmail.com>
2023-02-19 19:18:25 -0800

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints all the numbers of base 16 in lowercase
+ * Return: Always 0
+ */
+int main(void)
+{
+	char ln;
+
+	for (ln = '1'; ln <= '16'; ln++)
+		putchar(ln);
+
+	putchar('\n');
+
+	return (0);
+}

8cea2142379ddc815af551d00f4e7cfc95d4ba4c
ThexdxrBlck <morobe76o@gmail.com>
2023-02-19 19:11:01 -0800

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints alphabet in lower case format in reverse
+ * Return: Always 0
+ */
+int main(void)
+{
+	char lr;
+
+	for (lr = 'z'; lr > 'a'; lr--)
+		putchar(lr);
+
+	putchar('\n');
+
+	return (0);
+}
